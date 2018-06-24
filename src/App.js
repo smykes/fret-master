141c0aca80b3e2bfda5b4bd89e9c0b985a9b99e4
Jeffrey Smykil <jeff.smykil@gmail.com>
2018-06-24 14:49:55 -0500

    Reformatting

--- src/App.js
+++ src/App.js
@@ -1,140 +1,204 @@
-import React, { Component } from 'react';
-import './App.css';
-import './css/string-selector.css';
-import FretBoard from './components/fretBoard.jsx';
-import NoteDisplay from './components/noteDisplay.js';
-import DesiredDisplay from './components/desiredDisplay.js';
-import Footer from './components/footer.js';
-import StartScreen from './components/startScreen';
-import TuningSelector from './components/tuningSelector';
-import {Instruments} from './constants/instruments.js';
+import React, { Component } from "react";
+import "./App.css";
+import "./css/string-selector.css";
+import FretBoard from "./components/fretBoard.jsx";
+import NoteDisplay from "./components/noteDisplay.js";
+import DesiredDisplay from "./components/desiredDisplay.js";
+import Footer from "./components/footer.js";
+import StartScreen from "./components/startScreen";
+import TuningSelector from "./components/tuningSelector";
+import { Instruments } from "./constants/instruments.js";
 import {
-  getInsturmentNameByInstrumentId, 
-  getInstrumentByInstrumentId, 
-  getTuningNameByInstrumentIdAndTuningId, 
+  getInsturmentNameByInstrumentId,
+  getInstrumentByInstrumentId,
+  getTuningNameByInstrumentIdAndTuningId,
   getTuningByInstrumentIdAndTuningId,
   getNoteNameByInstrumentIdTuningIdStringNumberAndFretNumber,
   getFretNumberByInstrumentIdTuningIdStringNumberAndNoteName,
   getStringCountByInstrumentIdAndTuningId,
   getInstrumentList,
-  getRandomNumber, 
-  getNoteByNumber,
-} from './methods';
+  getRandomNumber,
+  getNoteByNumber
+} from "./methods";
 
 class App extends Component {
-    constructor(props) {
-        let instrumentId = 0;
-        let tuningId = 0;
-        super(props);
-        this.state = {
-            currentNote: '?',
-            desiredNote: 'C',
-            streak: 0,
-            chosen: false,
-            tuning: false,
-            ///////////////////
-            instrumentId,
-            tuningId,
-        };
+  constructor(props) {
+    let instrumentId = 0;
+    let tuningId = 0;
+    super(props);
+    this.state = {
+      currentNote: "?",
+      desiredNote: "C",
+      streak: 0,
+      chosen: false,
+      tuning: false,
+      ///////////////////
+      instrumentId,
+      tuningId
+    };
 
-        this.clickHandler = this.clickHandler.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.stringSelectorHandler = this.stringSelectorHandler.bind(this);
-        this.handleInstrumentSelection = this.handleInstrumentSelection.bind(this);
-        this.handleTuningSelection = this.handleTuningSelection.bind(this);
-        this.handleGoBack = this.handleGoBack.bind(this);
-    }
+    this.clickHandler = this.clickHandler.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.stringSelectorHandler = this.stringSelectorHandler.bind(this);
+    this.handleInstrumentSelection = this.handleInstrumentSelection.bind(this);
+    this.handleTuningSelection = this.handleTuningSelection.bind(this);
+    this.handleGoBack = this.handleGoBack.bind(this);
+  }
 
-    /*-- Event Handlers --*/
+  /*-- Event Handlers --*/
 
-    handleTuningSelection(tuningId) {
-      const desiredString = getTuningByInstrumentIdAndTuningId(this.state.instrumentId, tuningId);
-      this.setState({tuningId, tuning: true, desiredString: desiredString.stringCount - 1})
-    }
-    handleGoBack() {
-      this.setState({ tuning: false, chosen: false})
-    }
+  handleTuningSelection(tuningId) {
+    const desiredString = getTuningByInstrumentIdAndTuningId(
+      this.state.instrumentId,
+      tuningId
+    );
+    this.setState({
+      tuningId,
+      tuning: true,
+      desiredString: desiredString.stringCount - 1
+    });
+  }
+  handleGoBack() {
+    this.setState({ tuning: false, chosen: false });
+  }
 
-    clickHandler(notes, number, string, stringNumber) {
-      const clickedNote = getNoteNameByInstrumentIdTuningIdStringNumberAndFretNumber(this.state.instrumentId, this.state.tuningId, stringNumber, number);
-      if (clickedNote === this.state.desiredNote && stringNumber === this.state.desiredString) {
-        const numberOfStrings = getStringCountByInstrumentIdAndTuningId(this.state.instrumentId, this.state.tuningId);
-        const newDesiredString = this.state.desiredString - 1 >= 0 ? this.state.desiredString -1 : numberOfStrings - 1;
-        const newStreak = this.state.streak;
-        const randNo = getRandomNumber(0,11);
-        const randNote = getNoteByNumber(randNo);
+  clickHandler(notes, number, string, stringNumber) {
+    const clickedNote = getNoteNameByInstrumentIdTuningIdStringNumberAndFretNumber(
+      this.state.instrumentId,
+      this.state.tuningId,
+      stringNumber,
+      number
+    );
+    if (
+      clickedNote === this.state.desiredNote &&
+      stringNumber === this.state.desiredString
+    ) {
+      const numberOfStrings = getStringCountByInstrumentIdAndTuningId(
+        this.state.instrumentId,
+        this.state.tuningId
+      );
+      const newDesiredString =
+        this.state.desiredString - 1 >= 0
+          ? this.state.desiredString - 1
+          : numberOfStrings - 1;
+      const newStreak = this.state.streak;
+      const randNo = getRandomNumber(0, 11);
+      const randNote = getNoteByNumber(randNo);
 
-        this.setState({
-          currentNote: getNoteNameByInstrumentIdTuningIdStringNumberAndFretNumber(this.state.instrumentId, this.state.tuningId, stringNumber, number),
-          desiredString: newDesiredString,
-          desiredNote: randNote,
-          streak: newStreak + 1,
-        });
-      }
-      else {
-        this.error(getFretNumberByInstrumentIdTuningIdStringNumberAndNoteName(this.state.instrumentId, this.state.tuningId, this.state.desiredString, this.state.desiredNote), this.state.desiredString);
-      }
-    }
-
-    stringSelectorHandler(name, stringNumber) {
-      const activeStrings = this.state.activeStrings;
-      const index = (activeStrings.indexOf(stringNumber));
-      if (index !== -1) {
-        activeStrings.splice(index, 1);
-      }
-      else {
-        activeStrings.push(stringNumber);
-      }
-      this.setState({activeStrings: activeStrings.sort()}, () => {
-        console.log(this.state.activeStrings);
+      this.setState({
+        currentNote: getNoteNameByInstrumentIdTuningIdStringNumberAndFretNumber(
+          this.state.instrumentId,
+          this.state.tuningId,
+          stringNumber,
+          number
+        ),
+        desiredString: newDesiredString,
+        desiredNote: randNote,
+        streak: newStreak + 1
       });
+    } else {
+      this.error(
+        getFretNumberByInstrumentIdTuningIdStringNumberAndNoteName(
+          this.state.instrumentId,
+          this.state.tuningId,
+          this.state.desiredString,
+          this.state.desiredNote
+        ),
+        this.state.desiredString
+      );
     }
+  }
 
-    handleInstrumentSelection(id) {
-      this.setState({instrumentId: id, chosen: true,}, () => {
-        const instrument = getInstrumentByInstrumentId(id);
-        this.setState({ desiredString: (instrument.defaultStringCount-1), activeStrings: getTuningByInstrumentIdAndTuningId(id, this.state.tuningId)});
+  stringSelectorHandler(name, stringNumber) {
+    const activeStrings = this.state.activeStrings;
+    const index = activeStrings.indexOf(stringNumber);
+    if (index !== -1) {
+      activeStrings.splice(index, 1);
+    } else {
+      activeStrings.push(stringNumber);
+    }
+    this.setState({ activeStrings: activeStrings.sort() }, () => {
+      console.log(this.state.activeStrings);
+    });
+  }
+
+  handleInstrumentSelection(id) {
+    this.setState({ instrumentId: id, chosen: true }, () => {
+      const instrument = getInstrumentByInstrumentId(id);
+      this.setState({
+        desiredString: instrument.defaultStringCount - 1,
+        activeStrings: getTuningByInstrumentIdAndTuningId(
+          id,
+          this.state.tuningId
+        )
       });
-    }
+    });
+  }
 
-    handleChange(event) {
-      const instrument = Instruments[Instruments.map(e => e.instrumentName).indexOf(event.target.value)];
-      const startString = instrument.defaultStringCount - 1;
-      this.setState({instrument: instrument.instrumentName, currentNote: '?', desiredString: startString});
-    }
+  handleChange(event) {
+    const instrument =
+      Instruments[
+        Instruments.map(e => e.instrumentName).indexOf(event.target.value)
+      ];
+    const startString = instrument.defaultStringCount - 1;
+    this.setState({
+      instrument: instrument.instrumentName,
+      currentNote: "?",
+      desiredString: startString
+    });
+  }
 
-    error(fretNumber, desiredString) {
-      let correctFret = document.getElementsByClassName(`string-${desiredString} fret-${fretNumber}`)[0];
-      correctFret.className += ' correct-note';
-      this.setState({currentNote: '😭', streak: 0});
-    }
+  error(fretNumber, desiredString) {
+    let correctFret = document.getElementsByClassName(
+      `string-${desiredString} fret-${fretNumber}`
+    )[0];
+    correctFret.className += " correct-note";
+    this.setState({ currentNote: "😭", streak: 0 });
+  }
 
-    success() {}
+  success() {}
 
   render() {
     const allInstruments = getInstrumentList();
-    const chosen = this.state.chosen && this.state.tuning ? '' : 'hidden';
-    const tuning = this.state.chosen && !this.state.tuning ? '' : 'hidden';
-    const notChosen = this.state.chosen ? 'hidden' : '';
+    const chosen = this.state.chosen && this.state.tuning ? "" : "hidden";
+    const tuning = this.state.chosen && !this.state.tuning ? "" : "hidden";
+    const notChosen = this.state.chosen ? "hidden" : "";
     return (
       <div className="App">
         <div className={notChosen}>
-          <StartScreen instruments={allInstruments} handleInstrumentSelection={this.handleInstrumentSelection}/>
+          <StartScreen
+            instruments={allInstruments}
+            handleInstrumentSelection={this.handleInstrumentSelection}
+          />
         </div>
         <div className={tuning}>
-          <TuningSelector handleGoBack={this.handleGoBack} handleTuningSelection={this.handleTuningSelection} instrument={getInstrumentByInstrumentId(this.state.instrumentId)}/>
+          <TuningSelector
+            handleGoBack={this.handleGoBack}
+            handleTuningSelection={this.handleTuningSelection}
+            instrument={getInstrumentByInstrumentId(this.state.instrumentId)}
+          />
         </div>
-        <section className={chosen} id='game'>
+        <section className={chosen} id="game">
           <h4>
-            {getInsturmentNameByInstrumentId(this.state.instrumentId)} - {getTuningNameByInstrumentIdAndTuningId(this.state.instrumentId, this.state.tuningId)}
+            {getInsturmentNameByInstrumentId(this.state.instrumentId)} -{" "}
+            {getTuningNameByInstrumentIdAndTuningId(
+              this.state.instrumentId,
+              this.state.tuningId
+            )}
           </h4>
-          <h5>
-            Current Streak: {this.state.streak}
-          </h5>
-          <FretBoard clickHandler={this.clickHandler} instrumentId={this.state.instrumentId} tuningId={this.state.tuningId} desiredString={this.state.desiredString}/>
-          <div className={'note-container'}>
-            <DesiredDisplay note={this.state.desiredNote} stringNumber={this.state.desiredString + 1} />
-            <NoteDisplay note={this.state.currentNote}/>
+          <h5>Current Streak: {this.state.streak}</h5>
+          <FretBoard
+            clickHandler={this.clickHandler}
+            instrumentId={this.state.instrumentId}
+            tuningId={this.state.tuningId}
+            desiredString={this.state.desiredString}
+          />
+          <div className={"note-container"}>
+            <DesiredDisplay
+              note={this.state.desiredNote}
+              stringNumber={this.state.desiredString + 1}
+            />
+            <NoteDisplay note={this.state.currentNote} />
           </div>
         </section>
         <Footer />
--- src/methods.js
+++ src/methods.js
@@ -1,112 +1,147 @@
-import {InstrumentConstants} from './constants/instrumentConstants.js';
-import { Notes } from './constants/notes.js';
+import { InstrumentConstants } from "./constants/instrumentConstants.js";
+import { Notes } from "./constants/notes.js";
 
 /**
- * 
- * @param {number} min 
- * @param {number} max 
+ *
+ * @param {number} min
+ * @param {number} max
  */
 export function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 /**
- * 
- * @param {number} num 
+ *
+ * @param {number} num
  */
 export function getNoteByNumber(num) {
   return Notes[num];
 }
 
 /**
- * 
+ *
  */
 export function getInstrumentList() {
   return InstrumentConstants;
 }
 
 /**
- * 
- * @param {number} instrumentId 
+ *
+ * @param {number} instrumentId
  */
 export function getInstrumentByInstrumentId(instrumentId) {
-    return InstrumentConstants.find( instrument => instrument.instrumentId === instrumentId);
+  return InstrumentConstants.find(
+    instrument => instrument.instrumentId === instrumentId
+  );
 }
 
 /**
- * 
- * @param {number} instrumentId 
+ *
+ * @param {number} instrumentId
  */
 export function getInsturmentNameByInstrumentId(instrumentId) {
-  const instrument = InstrumentConstants.find( instrument => instrument.instrumentId === instrumentId);
+  const instrument = InstrumentConstants.find(
+    instrument => instrument.instrumentId === instrumentId
+  );
   return instrument.instrumentName;
 }
 
 /**
- * 
- * @param {number} instrumentId 
- * @param {number} tuningId 
+ *
+ * @param {number} instrumentId
+ * @param {number} tuningId
  */
 export function getTuningByInstrumentIdAndTuningId(instrumentId, tuningId) {
-  const instrument = InstrumentConstants.find( instrument => instrument.instrumentId === instrumentId);
-  return instrument.tunings.find( tuning => tuning.tuningId === tuningId);
+  const instrument = InstrumentConstants.find(
+    instrument => instrument.instrumentId === instrumentId
+  );
+  return instrument.tunings.find(tuning => tuning.tuningId === tuningId);
 }
 
 /**
- * 
- * @param {number} instrumentId 
- * @param {number} tuningId 
+ *
+ * @param {number} instrumentId
+ * @param {number} tuningId
  */
 export function getTuningNameByInstrumentIdAndTuningId(instrumentId, tuningId) {
-  const instrument = InstrumentConstants.find( instrument => instrument.instrumentId === instrumentId);
-  const tuning = instrument.tunings.find(tuning => tuning.tuningId === tuningId);
+  const instrument = InstrumentConstants.find(
+    instrument => instrument.instrumentId === instrumentId
+  );
+  const tuning = instrument.tunings.find(
+    tuning => tuning.tuningId === tuningId
+  );
   return tuning.tuningName;
 }
 
 /**
- * 
- * @param {number} instrumentId 
- * @param {number} tuningId 
+ *
+ * @param {number} instrumentId
+ * @param {number} tuningId
  */
-export function getStringCountByInstrumentIdAndTuningId(instrumentId, tuningId) {
-  const instrument = InstrumentConstants.find( instrument => instrument.instrumentId === instrumentId);
-  const tuning = instrument.tunings.find( tuning => tuning.tuningId === tuningId);
+export function getStringCountByInstrumentIdAndTuningId(
+  instrumentId,
+  tuningId
+) {
+  const instrument = InstrumentConstants.find(
+    instrument => instrument.instrumentId === instrumentId
+  );
+  const tuning = instrument.tunings.find(
+    tuning => tuning.tuningId === tuningId
+  );
   return tuning.stringCount;
 }
 
 /**
- * 
- * @param {number} instrumentId 
- * @param {number} tuningId 
- * @param {number} stringNumber 
- * @param {string} noteName 
+ *
+ * @param {number} instrumentId
+ * @param {number} tuningId
+ * @param {number} stringNumber
+ * @param {string} noteName
  */
-export function getFretNumberByInstrumentIdTuningIdStringNumberAndNoteName(instrumentId, tuningId, stringNumber, noteName) {
-  const instrument = InstrumentConstants.find( instrument => instrument.instrumentId === instrumentId );
-  const tuning = instrument.tunings.find( tuning => tuning.tuningId === tuningId);
+export function getFretNumberByInstrumentIdTuningIdStringNumberAndNoteName(
+  instrumentId,
+  tuningId,
+  stringNumber,
+  noteName
+) {
+  const instrument = InstrumentConstants.find(
+    instrument => instrument.instrumentId === instrumentId
+  );
+  const tuning = instrument.tunings.find(
+    tuning => tuning.tuningId === tuningId
+  );
   const stringName = tuning.stringNames[stringNumber].toUpperCase();
   const stringNameIndex = Notes.indexOf(stringName);
   const noteNameNameIndex = Notes.indexOf(noteName);
 
-  return ((noteNameNameIndex - stringNameIndex) < 1) ? (noteNameNameIndex - stringNameIndex) + 12 : (noteNameNameIndex - stringNameIndex);
+  return noteNameNameIndex - stringNameIndex < 1
+    ? noteNameNameIndex - stringNameIndex + 12
+    : noteNameNameIndex - stringNameIndex;
 }
 
 /**
- * 
- * @param {number} instrumentId 
- * @param {number} tuningId 
- * @param {number} stringNumber 
- * @param {number} fretNumber 
+ *
+ * @param {number} instrumentId
+ * @param {number} tuningId
+ * @param {number} stringNumber
+ * @param {number} fretNumber
  */
-export function getNoteNameByInstrumentIdTuningIdStringNumberAndFretNumber(instrumentId, tuningId, stringNumber, fretNumber) {
+export function getNoteNameByInstrumentIdTuningIdStringNumberAndFretNumber(
+  instrumentId,
+  tuningId,
+  stringNumber,
+  fretNumber
+) {
   const tuning = getTuningByInstrumentIdAndTuningId(instrumentId, tuningId);
   const stringName = tuning.stringNames[stringNumber].toUpperCase();
 
   // Where the string name falls in the chromatic scale
   const stringNameIndex = Notes.indexOf(stringName);
-  const newNoteIndex = stringNameIndex + fretNumber > 11 ? stringNameIndex + fretNumber -12 : stringNameIndex + fretNumber;
+  const newNoteIndex =
+    stringNameIndex + fretNumber > 11
+      ? stringNameIndex + fretNumber - 12
+      : stringNameIndex + fretNumber;
   const newNoteName = Notes[newNoteIndex];
 
   return newNoteName;
-
 }
